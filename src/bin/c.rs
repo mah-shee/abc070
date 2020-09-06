75ef363c86f503c95c86d2a6c01dac5945ddc785
masa <masatoshi.sboard@gmail.com>
2020-09-06 14:47:51 +0900

    Done c

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -1,8 +1,16 @@
+use num_integer::lcm;
 #[allow(unused_imports)]
 use proconio::marker::{Bytes, Chars, Usize1};
 use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        t: [usize; n],
     }
+    let mut ans = 1;
+    for &i in t.iter() {
+        ans = lcm(ans, i);
+    }
+    println!("{}", ans);
 }
